4e22ef589437d5ca1fd45c620bfe3e49321d7ae1
Guilherme <guilherme015-barbosa@hotmail.com>
2020-11-11 00:42:00 +0000

    create new user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,23 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-    def setup 
+  def setup 
     @user = User.new(name: "Test User", email: "user@test.com",
                      password: "P4$$w0rd", password_confirmation: "P4$$w0rd")
   end
-  
-  
+
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "name should be present" do
-    @user.name = "  "
+    @user.name = " "
     assert_not @user.valid?
   end
-  
-    test "email should be present" do
-    @user.email = "  "
+
+  test "email should be present" do
+    @user.email = " "
     assert_not @user.valid?
   end
   
@@ -25,20 +24,20 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "a" * 51
     assert_not @user.valid?
   end
-  
+
   test "email should not be too long" do
-    @user.email = "a" * 247 + "@test.com"
+    @user.email = "a" * 111 + "@test.com"
     assert_not @user.valid?
   end
-  
+
   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@test.com USER@test.COM U_SE-R@test.org.jp user2020+x@free.net]
+    valid_addresses = %w[User@test.com USER@test.com Use_ser@test.com user2020@test.org.br uSer2020+t@test.com.br]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@test,com user_at_test_com.us test.user@test. foo@bar_zaz.com]
     invalid_addresses.each do |invalid_address|
@@ -46,20 +45,21 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-  
-  test "email addressses should be unique" do
+
+   test "email addresses should be unique" do
     dup_user = @user.dup
     @user.save
     assert_not dup_user.valid?
-  end
-  
-  test "password should be present" do
+   end
+
+   test "password should be present" do
     @user.password = @user.password_confirmation = " " * 8
-    assert_not @user.valid?
-  end
-  
-  test "password should have a minimum length" do
+    assert_not @user.valid?   
+   end
+
+   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 7
     assert_not @user.valid?
-  end
+   end
+  
 end
